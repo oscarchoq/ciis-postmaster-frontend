177f63d77d3bfce3569a92815e8379e6784c8104
Oscar Choque <oscarch0qu3@gmail.com>
2025-05-05 00:14:56 -0500

    feat: portada principal

--- src/app/(postmaster)/page.tsx
+++ src/app/(postmaster)/page.tsx
@@ -1,5 +1,35 @@
+
+import { getBasePath } from '@/lib';
+import styles from '@/styles/Home.module.css'
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <h1>Web Postmaster</h1>
+    <>
+
+      <section id='home' className={styles.intro}>
+        <video 
+          className={styles.videofondo}
+          autoPlay
+          loop
+          muted
+          playsInline
+          >
+            <source src={getBasePath("/video1.mp4")} type='video/mp4' />
+            <track kind='captions' srcLang='es' label='Spanish Captions' default />
+        </video>
+        <div className={styles.introContainer}>
+          <Image
+            src={getBasePath("/logo-postmaster.png")}
+            alt='Postmaster Logo'
+            width={500}
+            height={500}
+            priority
+          />
+          <p className={styles.date}>21 de Agosto del 2025</p>
+        </div>
+      </section>
+
+    </>
   );
 }
---
+++ src/config/font.ts
@@ -0,0 +1,9 @@
+import { Roboto_Mono } from 'next/font/google'
+
+export const robotoMono100 = Roboto_Mono({ subsets: ['latin'], weight: '100' })
+export const robotoMono200 = Roboto_Mono({ subsets: ['latin'], weight: '200' })
+export const robotoMono300 = Roboto_Mono({ subsets: ['latin'], weight: '300' })
+export const robotoMono400 = Roboto_Mono({ subsets: ['latin'], weight: '400' })
+export const robotoMono500 = Roboto_Mono({ subsets: ['latin'], weight: '500' })
+export const robotoMono600 = Roboto_Mono({ subsets: ['latin'], weight: '600' })
+export const robotoMono700 = Roboto_Mono({ subsets: ['latin'], weight: '700' })
