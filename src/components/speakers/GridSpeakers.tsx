23c78dc2e9f00c831dc8a25c2f02d664ecd44b80
Oscar Choque <oscarch0qu3@gmail.com>
2025-05-07 12:37:16 -0500

    style(speaker)

--- src/components/speakers/GridSpeakers.tsx
+++ src/components/speakers/GridSpeakers.tsx
@@ -13,13 +13,15 @@ export const GridSpeakers = async () => {
   const speakers = await getSpeakers();
 
   return (
-    <section className='flex flex-col min-h-screen items-center justify-center text-white py-20 px-4'>
-      <div className={`text-center mb-12`}>
+
+    <section className='flex flex-col min-h-screen items-center justify-center text-white py-20 px-4 bg-gradient-to-b from-black/40 via-transparent to-transparent'>
+
+      <div className={`text-center mb-12 animate-fade-in-up`}>
         <h2 className='font-title text-4xl font-extrabold uppercase pb-2'>Ponentes</h2>
         <p className='text-lg sm:text-xl leading-tight text-zinc-300'>Egresados de la Escuela Profesional de Ingeniería en Informática y Sistemas</p>
       </div>
 
-      <div className='grid grid-cols-1 gap-x-7 gap-y-5 md:grid-cols-2 lg:grid-cols-3'>
+      <div className='grid grid-cols-1 gap-x-7 gap-y-5 md:grid-cols-2 lg:grid-cols-3 animate-fade-in-up'>
         {
           speakers.map(speaker => (
             <Speaker key={speaker.id} speaker={speaker} />
--- src/components/speakers/Speaker.tsx
+++ src/components/speakers/Speaker.tsx
@@ -1,6 +1,7 @@
-import { apiConfig, comfortaa } from "@/config";
+import { apiConfig } from "@/config";
 import { SpeakersResponse } from "@/interface";
 import Image from "next/image";
+import { FaLinkedin } from "react-icons/fa6";
 
 interface Props {
   speaker: SpeakersResponse
@@ -14,8 +15,7 @@ export const Speaker = ({ speaker }: Props) => {
         <div className="h-96 w-72">
           <Image
             src={apiConfig.domain + speaker.avatar}
-            width={500}
-            height={500}
+            fill
             alt='Speaker Image'
             className='w-full h-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125'
           />
@@ -23,34 +23,44 @@ export const Speaker = ({ speaker }: Props) => {
 
         <div
           className={`absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/70 group-hover:to-black/75`}
-          
+
         />
 
-        <div className={`${comfortaa.className} text-white absolute flex  flex-col gap-4 inset-0 items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 translate-y-[60%]`}>
+        <div className={`text-white absolute flex flex-col gap-4 inset-0 items-center justify-center px-3 text-center transition-all duration-500 group-hover:translate-y-0 translate-y-[60%]`}>
 
           {/* Nombres */}
-          <p className={`text-lg font-bold tracking-wider`}>
+          <p className={`text-lg font-bold tracking-wider font-title leading-none`}>
             <span className="block">{speaker.name}</span>
             <span className="block">{speaker.lastname}</span>
           </p>
 
           {/* descripcion */}
-          <div className='group min-h-28 group-hover:min-h-0'>
+          <div className='group min-h-24 group-hover:min-h-0'>
             <p
-              className={`text-base opacity-0 transition-opacity duration-300 group-hover:opacity-100`}
+              className={`text-base opacity-0 transition-opacity duration-300 group-hover:opacity-100 hidden group-hover:block`}
             >
               {speaker.description}
             </p>
           </div>
 
           {/* Perfil LinkedIn */}
-          <a
-            className={`text-xs rounded-full bg-neutral-900 py-2 px-3.5 shadow shadow-black/60 cursor-pointer`}
-            href={speaker.socialNetwork}
-            target="_blank"
-          >
-            LinkedIn
-          </a>
+          <div className="pointer-events-none group-hover:pointer-events-auto group/button">
+            <a
+              href={speaker.socialNetwork}
+              target="_blank"
+              className="text-xs rounded-full py-2 px-6 opacity-0 group-hover:opacity-100 transition-all duration-500
+               group-hover:pointer-events-auto pointer-events-none
+               relative inline-flex items-center justify-center overflow-hidden 
+                bg-[#000126] font-medium text-neutral-200"
+            >
+              <div className="translate-x-0 opacity-100 transition group-hover/button:-translate-x-[150%] group-hover/button:opacity-0">
+                Ver perfil
+              </div>
+              <div className="absolute translate-x-[150%] opacity-0 transition group-hover/button:translate-x-0 group-hover/button:opacity-100 flex items-center justify-center gap-2 text-xs">
+                <FaLinkedin /> LinkedIn
+              </div>
+            </a>
+          </div>
 
         </div>
 
